abceff93351d9d8473cb2949aa0b368e6c2435c7
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-03 09:31:09 +0300

    logic

---
+++ express/Logic/VideoLogicMongo.ts
@@ -0,0 +1,28 @@
+import Song from '../Models/Song'
+
+// add song, update song,delete song,get song by id, get all songs
+// one time running: create table songs, create table categories
+
+const addSong = (newSong: Song) => {}
+
+const updateSong = (song: Song) => {}
+
+const deleteSong = (id: number) => {}
+
+const getSongById = (id: number) => {}
+
+const getAllSongs = () => {}
+
+const createSongsTable = () => {}
+
+const createCategoriesTable = () => {}
+
+export default {
+  addSong,
+  updateSong,
+  deleteSong,
+  getSongById,
+  getAllSongs,
+  createSongsTable,
+  createCategoriesTable,
+}
